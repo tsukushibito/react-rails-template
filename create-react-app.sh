910b8956edf321273ab06d13bd2131ff3d629b22
tsukushibito <tsukushibito@gmail.com>
2021-05-19 21:46:55 +0900

    Add react application

--- create-react-app.sh
+++ create-react-app.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker compose run --rm web sh -c "npm install -g create-react-app && create-react-app ."
+. .env
+docker-compose run --rm web sh -c "npm install -g create-react-app && create-react-app ."
